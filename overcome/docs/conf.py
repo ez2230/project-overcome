061ef76287b88fcd54e65d778a93d80581e66824
overcome.site <overcome.site@gmail.com>
2015-03-12 21:29:50 -0700

    add RTD setup for custom css

--- overcome/docs/conf.py
+++ overcome/docs/conf.py
@@ -291,3 +291,6 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+def setup(app):
+    app.add_stylesheet('css/extras.css')
